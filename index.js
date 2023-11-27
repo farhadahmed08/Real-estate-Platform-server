4f2e11a79ecb3b087736881aab72ba67a029d7db
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-28 02:36:24 +0800

    connected to the mongodb

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
+const { MongoClient } = require('mongodb');
 
 
 //middleware
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = "mongodb://0.0.0.0:27017/";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 
 app.get('/',(req,res)=>{
