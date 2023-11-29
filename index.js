ce8ae77f19e92cbbd07968a8f55df7f0ec163553
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-29 19:29:20 +0800

    user data store in database and jwt token impliment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 const { MongoClient } = require('mongodb');
@@ -23,6 +24,51 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("realEstateDb").collection("users");
+
+
+    //jwt related api
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:'1h'
+      });
+      console.log(token)
+      // res.send({token:token})
+      res.send({token}); //short hand
+    })
+
+
+
+     //user related api
+     app.get('/users', async(req,res)=>{
+      
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users',async (req,res)=>{
+      const user = req.body;
+      // insert email if user dosent exist 
+      // you can do this many ways (1.email unique, 2.upsert 3.simple checking)
+      const query = {email:user.email}
+      // console.log(query)
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({message:'user already exists',insertedId:null})
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
