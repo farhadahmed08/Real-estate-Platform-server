943963c84f3472e189582a296da0e46722fa4714
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-30 18:43:45 +0800

    verify token

--- index.js
+++ index.js
@@ -42,6 +42,24 @@ async function run() {
       res.send({token}); //short hand
     })
 
+    //middleware
+    const verifyToken = (req,res,next)=>{
+      console.log('inside verify token',req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({message:'unauthorized access'})
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if (err) {
+          return res.status(401).send({message:'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+     
+    }
+
+
 
 
      //user related api
