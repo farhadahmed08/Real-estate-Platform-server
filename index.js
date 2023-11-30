d2eaebe6791719f8f939ca1c93989fa8f2017b55
Md Farhad Ahmed <farhaddhkbd08@gmail.com>
2023-11-30 18:47:31 +0800

    verify admin

--- index.js
+++ index.js
@@ -61,6 +61,21 @@ async function run() {
 
 
 
+    //use verify admin after verifyToken
+    
+    const verifyAdmin = async(req,res,next)=>{
+      const email = req.decoded.email;
+      const query = {email:email} 
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({message:'forbidden access'})
+      }
+      next();
+    }
+
+
+
 
      //user related api
      app.get('/users', async(req,res)=>{
